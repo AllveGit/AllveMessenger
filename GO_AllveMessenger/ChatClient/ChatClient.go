dda91a8ace02d0728a875421bfa55067aff713b6
AllveGit <createpubpriv@gmail.com>
2019-12-11 18:54:23 +0900

    AllveMessenger GO Ver

---
+++ GO_AllveMessenger/ChatClient/ChatClient.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"os"
+)
+
+var serverAndPort string = "127.0.0.1:9999"
+var quit bool = false
+
+type messagePacket struct {
+	messageBufferSize [8]byte
+	message           string
+}
+
+func errorCheck(errorLog error) bool {
+
+	if errorLog != nil {
+		fmt.Println(errorLog)
+		return false
+	}
+
+	return true
+}
+
+func SendMessage(connect net.Conn, sendChannelDone *chan bool) {
+
+	var packet messagePacket
+
+	var messageBufferSize [8]byte
+	var packetSize int
+
+	for !quit {
+		// fmt.Scan(&packet.message)
+		in := bufio.NewReader(os.Stdin)
+		line, err := in.ReadString('\n')
+
+		if !errorCheck(err) {
+			quit = true
+			break
+		}
+
+		packet.message = line
+		packetSize = len(packet.message)
+		// fmt.Println(packetSize)
+
+		binary.BigEndian.PutUint64(messageBufferSize[:], uint64(packetSize))
+		_, bufErr := connect.Write(messageBufferSize[:])
+
+		if !errorCheck(bufErr) {
+			quit = true
+			break
+		}
+
+		_, msgErr := connect.Write([]byte(packet.message))
+
+		if !errorCheck(msgErr) {
+			quit = true
+			break
+		}
+	}
+
+	*sendChannelDone <- true
+}
+
+func ReadMessage(connect net.Conn, readChannelDone *chan bool) {
+
+	var messageBufferSize [8]byte
+	var packetSize int
+
+	for !quit {
+		_, bufErr := connect.Read(messageBufferSize[:])
+
+		if !errorCheck(bufErr) {
+			quit = true
+			break
+		}
+
+		packetSize = int(binary.BigEndian.Uint64(messageBufferSize[:]))
+		var messageData string = ""
+
+		recvBuf := make([]byte, packetSize)
+
+		for 0 < packetSize {
+			_, msgErr := connect.Read(recvBuf)
+
+			if !errorCheck(msgErr) {
+				quit = true
+				break
+			}
+
+			messageData = string(recvBuf)
+
+			packetSize -= len(recvBuf)
+		}
+
+		fmt.Println(messageData)
+	}
+
+	*readChannelDone <- true
+}
+
+func TryConnect(_serverAndPort string) net.Conn {
+	connect, err := net.Dial("tcp", _serverAndPort)
+
+	if !errorCheck(err) {
+		return nil
+	}
+
+	return connect
+}
+
+func main() {
+	var nickname string
+	fmt.Print("닉네임을 입력하세요 : ")
+	fmt.Scanln(&nickname)
+
+	connect := TryConnect(serverAndPort)
+
+	defer connect.Close()
+
+	fmt.Println("서버에 연결 성공하였습니다")
+
+	sendChannelEnd := make(chan bool)
+	readChannelEnd := make(chan bool)
+
+	go SendMessage(connect, &sendChannelEnd)
+	go ReadMessage(connect, &readChannelEnd)
+
+	<-sendChannelEnd
+	<-readChannelEnd
+
+	close(sendChannelEnd)
+	close(readChannelEnd)
+
+	return
+}
---
+++ GO_AllveMessenger/ChatServer/ChatServer.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+)
+
+const (
+	MAX_USER_NUM = 64
+)
+
+var MessageChannel chan string
+var QuitChannel chan Client
+
+type messagePacket struct {
+	messageBufferSize [8]byte
+	message           string
+}
+
+type Client struct {
+	connect net.Conn
+	end     bool
+}
+
+var serverAndPort string = "127.0.0.1:9999"
+var users map[Client]bool
+var curUserNum int = 0
+
+var CriticalSection sync.WaitGroup
+
+func ReadMessage(user *Client) string {
+
+	var messageBufferSize [8]byte
+	var packetSize int
+
+	var messageData string = ""
+
+	_, bufErr := user.connect.Read([]byte(messageBufferSize[:]))
+
+	if !errorCheck(bufErr) {
+		user.end = true
+	}
+
+	packetSize = int(binary.BigEndian.Uint64(messageBufferSize[:]))
+
+	recvBuf := make([]byte, packetSize)
+
+	for 0 < packetSize {
+		_, msgErr := user.connect.Read(recvBuf)
+
+		if !errorCheck(msgErr) {
+			user.end = true
+			break
+		}
+
+		messageData = string(recvBuf)
+
+		packetSize -= len(messageData)
+	}
+
+	return messageData
+}
+
+func SendMessage(user *Client, message string) {
+	var packet messagePacket
+
+	var messageBufferSize [8]byte
+	var packetSize int
+
+	packet.message = message
+	packetSize = len(packet.message)
+	fmt.Println(packetSize)
+
+	binary.BigEndian.PutUint64(messageBufferSize[:], uint64(packetSize))
+	_, bufErr := user.connect.Write(messageBufferSize[:])
+
+	if !errorCheck(bufErr) {
+		user.end = true
+		return
+	}
+
+	_, msgErr := user.connect.Write([]byte(packet.message))
+
+	if !errorCheck(msgErr) {
+		user.end = true
+		return
+	}
+}
+
+func BroadCast(message string) {
+	for key, _ := range users {
+		SendMessage(&key, message)
+	}
+}
+
+func ManageUser() {
+
+	for {
+		// for QuitClient := range QuitChannel {
+		// 	delete(users, QuitClient)
+		// 	QuitClient.end = true
+		// }
+
+		// for Messages := range MessageChannel {
+		// 	BroadCast(Messages)
+		// }
+	}
+}
+
+func ClientReadMessage(user *Client) {
+	defer user.connect.Close()
+
+	for !user.end {
+		readedMessage := ReadMessage(user)
+
+		if strings.Compare(readedMessage, "quit\r\n") == 0 {
+			QuitChannel <- *user
+		} else {
+			fmt.Println(readedMessage)
+			// MessageChannel <- readedMessage
+
+			BroadCast(readedMessage)
+		}
+	}
+}
+
+func errorCheck(errorLog error) bool {
+
+	if errorLog != nil {
+		fmt.Println(errorLog)
+		return false
+	}
+
+	return true
+}
+
+func StartServer(_serverAndPort string) net.Listener {
+
+	users = make(map[Client]bool)
+
+	MessageChannel = make(chan string, MAX_USER_NUM)
+	QuitChannel = make(chan Client, MAX_USER_NUM)
+
+	server, err := net.Listen("tcp", _serverAndPort)
+
+	if !errorCheck(err) {
+		return nil
+	}
+
+	return server
+}
+
+func main() {
+	server := StartServer(serverAndPort)
+
+	defer server.Close()
+
+	go ManageUser()
+
+	for {
+		newClient, err := server.Accept()
+
+		if !errorCheck(err) {
+			return
+		}
+
+		fmt.Println("새로운 클라이언트가 서버에 들어왔습니다")
+
+		processClient := Client{newClient, false}
+
+		users[processClient] = true
+
+		go ClientReadMessage(&processClient)
+	}
+}
